a4c7b96a39c193f14db6c6f1dea8a4309a253522
artlenk.ru@gmail.com <df5ukoi7ht>
2017-01-03 11:01:27 +0300

    Compress images and fixed ig logo partner. Added .gitignore. Fixed css bug. Fixed modal bug

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,3 @@
-// Актальный год в подвале сайта
-/*
- var dateMy = new Date();
- var year = dateMy.getFullYear();
- document.getElementById('year').innerHTML = year;
- */
-
 $(document).ready(function () {
 
 // Вывод актуального года в подвал сайта
@@ -12,29 +5,12 @@ $(document).ready(function () {
     var currentYear = dateMy.getFullYear();
     document.getElementById('current-year').innerHTML = currentYear;
 
-    //Плавная прокрутка к якорям на странице
-    $("body").on('click', '[href*="#"]', function (e) {
-        var fixed_offset = 100;
-        $('html,body').stop().animate({scrollTop: $(this.hash).offset().top - fixed_offset}, 1000);
-        e.preventDefault();
-    });
-
     // Yep, that's it!
     $('#scene').parallax();
 
-
-    //@TODO delete
-/*    $('.timer').countTo({
-        speed: 4000,
-        refreshInterval: 60,
-        formatter: function (value, options) {
-            return value.toFixed(options.decimals);
-        }
-    });*/
-
-    $('.statistic').appear(function() {
+    $('.statistic').appear(function () {
         $('.timer').countTo({
-            speed: 4000,
+            speed: 5000,
             refreshInterval: 60,
             formatter: function (value, options) {
                 return value.toFixed(options.decimals);
